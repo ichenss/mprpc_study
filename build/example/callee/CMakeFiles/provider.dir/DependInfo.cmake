a015acc5b6c55d29219d151f8e6c2851a0030f7a
chen <1773104733@qq.com>
2024-04-01 21:06:13 +0800

    项目动态库编译

--- build/example/callee/CMakeFiles/provider.dir/DependInfo.cmake
+++ build/example/callee/CMakeFiles/provider.dir/DependInfo.cmake
@@ -14,6 +14,7 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yuhang/mprpc_study/build/src/CMakeFiles/mprpc.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
---
+++ build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o.d
@@ -0,0 +1,4 @@
+src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o: \
+ /home/yuhang/mprpc_study/src/mprpcapplication.cc \
+ /usr/include/stdc-predef.h \
+ /home/yuhang/mprpc_study/src/include/mprpcapplication.h
