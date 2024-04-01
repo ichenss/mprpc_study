642c541cf0de2dac1381946170d9960f16414748
chen <1773104733@qq.com>
2024-04-01 18:09:09 +0800

    本地服务发布rpc服务2

--- example/callee/userservice.cc
+++ example/callee/userservice.cc
@@ -3,18 +3,17 @@
 
 #include "user.pb.h"
 
-using namespace std;
-
 /*
 UserService原来是一个本地服务，提供了两个进程内的本地方法，Login和GetFriendLists
 */
 class UserService : public fixbug::UserServiceRpc // 使用在rpc服务发布端 (rpc服务提供者)
 {
 public:
-    bool Login(string name, string pwd)
+    bool Login(std::string name, std::string pwd)
     {
-        cout << "doing local service: Login " << endl;
-        cout << "name: " << name << "pwd: " << pwd << endl;
+        std::cout << "doing local service: Login " << std::endl;
+        std::cout << "name: " << name << "pwd: " << pwd << std::endl;
+        return true;
     }
 
     // 重写基类UserServiceRpc的虚函数，下面这些方法都是框架直接调用的
@@ -23,7 +22,20 @@ public:
                        ::fixbug::LoginResponse* response,
                        ::google::protobuf::Closure* done)
     {
-        
+        std::string name = request->name();
+        std::string pwd = request->pwd();
+
+        // 做本地业务
+        bool login_result = Login(name, pwd);
+
+        // 把响应写入, 包括错误码、错误消息以及返回值
+        fixbug::ResultCode *code = response->mutable_result();
+        code->set_errcode(0);
+        code->set_errmsg("");
+        response->set_success(login_result);
+
+        // 执行回调操作, 执行响应对象数据的序列化和网络发送(都是由框架来完成的)
+        done->Run();
     }
 
 };
